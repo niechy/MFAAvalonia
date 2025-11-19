c9f023cc1741a45526326ba4658ff8137a405788
SweetSmellFox <1254371679@qq.com>
2025-11-19 13:47:36 +0800

    chore: 更新依赖版本并升级成.NET 10

--- MFAAvalonia/Helper/ToastNotification.cs
+++ MFAAvalonia/Helper/ToastNotification.cs
@@ -1,50 +1,51 @@
 ﻿using Avalonia.Controls.Notifications;
-using DesktopNotifications.Apple;
-using DesktopNotifications.FreeDesktop;
-using DesktopNotifications.Windows;
+// using DesktopNotifications.Apple;
+// using DesktopNotifications.FreeDesktop;
+// using DesktopNotifications.Windows;
 using MFAAvalonia.Helper;
 using System;
 using System.Collections.Generic;
-using INotificationManager = DesktopNotifications.INotificationManager;
-using Notification = DesktopNotifications.Notification;
+// using INotificationManager = DesktopNotifications.INotificationManager;
+// using Notification = DesktopNotifications.Notification;
 
 namespace MFAAvalonia.Helper;
 
 public static class ToastNotification
 {
-    private static INotificationManager? GetNotificationManager()
-    {
-
-        if (OperatingSystem.IsWindows())
-        {
-            return new WindowsNotificationManager();
-        }
-        // if (OperatingSystem.IsMacOS() || OperatingSystem.IsIOS())
-        // {
-        //     return new AppleNotificationManager();
-        // }
-        if (OperatingSystem.IsLinux() || OperatingSystem.IsFreeBSD())
-        {
-            return new FreeDesktopNotificationManager();
-        }
-
-        return null;
-    }
+    // private static INotificationManager? GetNotificationManager()
+    // {
+    //
+    //     if (OperatingSystem.IsWindows())
+    //     {
+    //         return new WindowsNotificationManager();
+    //     }
+    //     // if (OperatingSystem.IsMacOS() || OperatingSystem.IsIOS())
+    //     // {
+    //     //     return new AppleNotificationManager();
+    //     // }
+    //     if (OperatingSystem.IsLinux() || OperatingSystem.IsFreeBSD())
+    //     {
+    //         return new FreeDesktopNotificationManager();
+    //     }
+    //
+    //     return null;
+    // }
 
     public static void Show(string title = "", string message = "")
     {
 
         try
         {
-            var notificationManager = GetNotificationManager();
-
-            var notification = new Notification
-            {
-                Title = title,
-                Body = message
-            };
-
-            notificationManager?.ShowNotification(notification);
+            // var notificationManager = GetNotificationManager();
+            //
+            // var notification = new Notification
+            // {
+            //     Title = title,
+            //     Body = message
+            // };
+            //
+            // notificationManager?.ShowNotification(notification);
+            ToastHelper.Info(title, message);
         }
         catch (Exception e)
         {
--- MFAAvalonia/Views/UserControls/Settings/ExternalNotificationSettingsUserControl.axaml.cs
+++ MFAAvalonia/Views/UserControls/Settings/ExternalNotificationSettingsUserControl.axaml.cs
@@ -14,3 +14,4 @@ public partial class ExternalNotificationSettingsUserControl : UserControl
     }
 }
 
+ 
